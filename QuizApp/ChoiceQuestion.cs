fb8b4983ca621d84f3e6f299a318e2f49f3e74bc
Miladin Jeremic <miladin@live.nl>
2017-11-15 14:38:22 +0100

    Added multiple choice questions

---
+++ QuizApp/ChoiceQuestion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp
+{
+    class ChoiceQuestion : Question
+    {
+        private List<string> Choices = new List<string>();
+
+        public void AddChoice(string Choice, bool Correct)
+        {
+            Choices.Add(Choice);
+            if (Correct)
+            {
+                string ChoiceString = "" + Choices.Count;
+                Answer= ChoiceString;
+            }
+                
+        }
+
+        public new void Display()
+        {
+            for (int i = 0; i < Choices.Count; i++)
+            {
+                int ChoiceNumber = i + 1;
+                Console.WriteLine(ChoiceNumber + ": " + Choices[i]);
+            }
+        }
+
+    }
+}
--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -6,21 +6,47 @@ namespace QuizApp
     {
         static void Main(string[] args)
         {
+            
+            //Question first = new Question();
+            //first.Text = "Who was the inventor of Java?" ;
+            //first.Answer = "James Gosling";
 
-            Question first = new Question();
-            first.Text = "Who was the inventor of Java?" ;
-            first.Answer = "James Gosling";
 
 
+            //first.Display();
+            //Console.WriteLine("Your answer: ");
+            //string Response = Console.ReadLine();
 
-            first.display();
-            Console.WriteLine("Your answer: ");
-            string Response = Console.ReadLine();
+            //Console.WriteLine(first.CheckAnswer(Response));
 
-            Console.WriteLine(first.CheckAnswer(Response));
 
+            ChoiceQuestion cq1= new ChoiceQuestion();
+            cq1.Text = "What was the original name of the java language?";
+            cq1.AddChoice("7", false);
+            cq1.AddChoice("Duke", false);
+            cq1.AddChoice("Oak", true);
+            cq1.AddChoice("Gosling", false);
+
+            ChoiceQuestion cq2 = new ChoiceQuestion();
+            cq2.Text = "In which country was the inventor of java born?";
+            cq2.AddChoice("Australia", false);
+            cq2.AddChoice("England", true);
+            cq2.AddChoice("'Murica", false);
+
+            PresentQuestion(cq1);
+            PresentQuestion(cq2);
+            
+           
 
             Console.ReadLine();
+        }
+
+        public static void PresentQuestion(ChoiceQuestion q)
+        {
+            q.Display();
+            Console.WriteLine("Your answer: ");
+            string response = Console.ReadLine();
+            Console.WriteLine(q.CheckAnswer(response));
         }
     }
 }
